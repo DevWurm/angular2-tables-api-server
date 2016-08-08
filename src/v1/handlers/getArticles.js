dd7936fd423400bc88fd29bcb4d2b13863b9ef66
DevWurm <devwurm@devwurm.net>
2016-08-08 12:41:10 +0200

    Adding articles endpoint

---
+++ src/v1/handlers/getArticles.js
@@ -0,0 +1,26 @@
+import parseRequest from "../queries/counts/parsing";
+import getCurrentCollection from "../database/getCurrentCollection";
+import getArticlesData from "../data/getArticlesData";
+
+/**
+ * Handler for the counts get route
+ * Parses the qury and responds with the querried data
+ *
+ * @access public
+ *
+ * @param req {Request} Express request object for the current request
+ * @param res {Response} Express response object for the current request
+ * @param next {Function} Express next middleware function
+ */
+export default function getArticles (req, res, next) {
+    const queries = parseRequest(req);
+    
+    getCurrentCollection().then(col => {
+        return getArticlesData(queries, col);
+    }).then(data => {
+        res.json(data);
+        res.end()
+    }).catch(reason => {
+        next(reason);
+    })
+}
--- src/v1/routes/apiRoutes.js
+++ src/v1/routes/apiRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import getCounts from "../handlers/getCounts";
+import getArticles from "../handlers/getArticles";
+
 
 const router = express.Router();
 
+router.get("/articles", getArticles);
 router.get("/counts", getCounts)
 
 export default router;

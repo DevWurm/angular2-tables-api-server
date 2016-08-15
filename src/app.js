8a50d3a2ae8ccc79a445f9f440b03b6fd4a5be74
DevWurm <devwurm@devwurm.net>
2016-08-15 18:59:18 +0200

    Adding CQRS support

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from 'cors';
 
 import v1ApiRoutes from "./v1/routes/apiRoutes";
 
 // setup express application
 const app = express();
+
+// setup Cross-Origin-Resource-Sharing (CORS)
+app.use(cors());
 
 // setup routes
 app.use("/api/v1/", v1ApiRoutes);

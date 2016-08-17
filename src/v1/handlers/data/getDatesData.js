b80ba520cec696d417f3e89918c4b32b4abc85eb
DevWurm <devwurm@devwurm.net>
2016-08-17 17:09:12 +0200

    Adding dates route
    * the route provides an Array containing all availiable dates

---
+++ src/v1/handlers/data/getDatesData.js
@@ -0,0 +1,22 @@
+// @flow
+
+import type { MongoCollection } from "../../database/getCurrentCollection";
+
+/**
+ * collects dates array from the specified DB dates collection, which match the specified queries
+ *
+ * @access public
+ *
+ * @param queries {Object} Object containing information about selected ranges and sorting
+ * @param col {Collection} MongoDB collection object
+ *
+ * @return {Promise} Promise resolved with the Object representation of the requested data or rejected with errors while querying the database
+ */
+export default function getCountData(col: MongoCollection) {
+  return new Promise((resolve, reject) => {
+    col.findOne({ _id: 'dates' }, { _id: false }, (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    })
+  });
+}
--- src/v1/routes/apiRoutes.js
+++ src/v1/routes/apiRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import getCounts from "../handlers/getCounts";
 import getArticles from "../handlers/getArticles";
+import getDates from "../handlers/getDates";
 
 
 const router = express.Router();
 
 router.get("/articles", getArticles);
 router.get("/counts", getCounts)
+router.get("/dates", getDates);
 
 export default router;

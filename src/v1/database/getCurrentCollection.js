c7d3018295025f9837c02494f1be19cd5599c471
DevWurm <devwurm@devwurm.net>
2016-08-17 13:14:55 +0200

    Adding flow type annotations

--- src/v1/database/getCurrentCollection.js
+++ src/v1/database/getCurrentCollection.js
@@ -1,4 +1,8 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
+
+export type MongoCollection = {
+  aggregate: Function
+}
 
 /**
  * gets a connection to the specified database and collection defined in the environment
@@ -7,19 +11,18 @@ import {MongoClient} from "mongodb";
  *
  * @return {Promise} Promise resolved with the mongodb collection, specified by the environment or rejected with errors while connecting with the database
  */
+export default function getCurrentCollection(): MongoCollection {
+  const addr = process.env.DB_ADDR || "127.0.0.1";
+  const port = process.env.DB_PORT || "27017";
 
-export default function getCurrentCollection() {
-    const addr = process.env.DB_ADDR || "127.0.0.1";
-    const port = process.env.DB_PORT || "27017";
+  const db = process.env.DB_DB || "wikipedia"
+  const col = process.env.DB_COL || "pagecounts";
 
-    const db = process.env.DB_DB || "wikipedia"
-    const col = process.env.DB_COL || "pagecounts";
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(`mongodb://${addr}:${port}/${db}`, (err, db) => {
+      if (err) return reject(err);
 
-    return new Promise((resolve, reject) => {
-        MongoClient.connect(`mongodb://${addr}:${port}/${db}`, (err, db) => {
-            if (err) return reject(err);
-
-            resolve(db.collection(col));
-        })
-    });
+      resolve(db.collection(col));
+    })
+  });
 }

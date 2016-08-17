e08b5d1c001858d36b5cbe2ab4385ef9ac9bac77
Lindhorst <leo.lindhorst@saxsys.de>
2016-08-17 16:58:58 +0200

    Refactoring getCurrentCollection module
    * the module now provides access to the dates collection

--- src/v1/database/getCurrentCollection.js
+++ src/v1/database/getCurrentCollection.js
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb";
 
 export type MongoCollection = {
-  aggregate: Function
+  aggregate: Function,
+  findOne: Function
 }
 
 /**
@@ -11,14 +12,35 @@ export type MongoCollection = {
  *
  * @return {Promise} Promise resolved with the mongodb collection, specified by the environment or rejected with errors while connecting with the database
  */
-export default function getCurrentCollection(): MongoCollection {
+export default function getCurrentCollection(): Promise<MongoCollection> {
   const addr = process.env.DB_ADDR || "127.0.0.1";
   const port = process.env.DB_PORT || "27017";
 
   const db = process.env.DB_DB || "wikipedia"
   const col = process.env.DB_COL || "pagecounts";
 
-  return new Promise((resolve, reject) => {
+  return getCollection(addr, port, db, col);
+}
+
+/**
+ * gets a connection to the specified database and dates collection defined in the environment
+ *
+ * @access public
+ *
+ * @return {Promise} Promise resolved with the mongodb collection, specified by the environment or rejected with errors while connecting with the database
+ */
+export function getCurrentDatesCollection(): Promise<MongoCollection> {
+  const addr = process.env.DB_ADDR || "127.0.0.1";
+  const port = process.env.DB_PORT || "27017";
+
+  const db = process.env.DB_DB || "wikipedia"
+  const col = process.env.DB_DATES_COL || "pagecounts_dates";
+
+  return getCollection(addr, port, db, col);
+}
+
+function getCollection(addr, port, db, col): Promise<MongoCollection> {
+   return new Promise((resolve, reject) => {
     MongoClient.connect(`mongodb://${addr}:${port}/${db}`, (err, db) => {
       if (err) return reject(err);
 

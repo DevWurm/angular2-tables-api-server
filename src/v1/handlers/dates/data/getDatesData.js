5e39b479b472f118111cfdc880ea626d0be97dd5
DevWurm <devwurm@devwurm.net>
2016-08-25 17:29:42 +0200

    Switching dates endpoint to Elasticsearch ackend

--- src/v1/handlers/dates/data/getDatesData.js
+++ src/v1/handlers/dates/data/getDatesData.js
@@ -1,22 +1,39 @@
 // @flow
+import type { ElasticsearchConnection } from "../../../database/getESConnection";
 
-import type { MongoCollection } from "../../../database/getCurrentCollection";
 
 /**
  * collects dates array from the specified DB dates collection, which match the specified queries
  *
  * @access public
  *
- * @param queries {Object} Object containing information about selected ranges and sorting
- * @param col {Collection} MongoDB collection object
+ * @param client {ElasticsearchClient} client which should be used for connections to ES
+ * @param index {string} Elasticsearch index, which should be used
+ * @param type {string} Elasticsearch type, which should be used
  *
  * @return {Promise} Promise resolved with the Object representation of the requested data or rejected with errors while querying the database
  */
-export default function getCountData(col: MongoCollection) {
-  return new Promise((resolve, reject) => {
-    col.findOne({ _id: 'dates' }, { _id: false }, (err, data) => {
-      if (err) return reject(err);
-      resolve(data);
-    })
-  });
+export default function getDatesData({ client, index, type }: ElasticsearchConnection) {
+  return client.search({
+    index: index,
+    type: type,
+    size: 0,
+    body: {
+      "aggs": {
+        "dates": {
+          "nested": {
+            "path": "counts"
+          },
+          "aggs": {
+            "dates": {
+              "terms": {
+                "field": "counts.date",
+                "size": 0
+              }
+            }
+          }
+        }
+      }
+    }
+  }).then(data => data.aggregations.dates.dates.buckets).then(buckets => buckets.map(bucket => bucket.key_as_string));
 }
--- src/v1/handlers/dates/getDates.js
+++ src/v1/handlers/dates/getDates.js
@@ -1,5 +1,6 @@
 import { getCurrentDatesCollection } from "../../database/getCurrentCollection";
 import getDatesData from "./data/getDatesData";
+import { getESConnection } from "../../database/getESConnection";
 
 /**
  * Handler for the dates get route
@@ -11,12 +12,11 @@ import getDatesData from "./data/getDatesData";
  * @param next {Function} Express next middleware function
  */
 export default function getDates (req, res, next) {
-    getCurrentDatesCollection().then(col => {
-        return getDatesData(col);
-    }).then(data => {
-        res.json(data);
-        res.end()
-    }).catch(reason => {
+    getDatesData(getESConnection())
+      .then(data => {
+          res.json(data);
+          res.end()
+      }).catch(reason => {
         next(reason);
     })
 }
